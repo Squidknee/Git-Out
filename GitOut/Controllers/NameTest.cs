868a5d38cd670dcfbb5aa10314ac6851d72cfb6f
Squidknee <72271842+Squidknee@users.noreply.github.com>
2023-10-31 18:23:37 -0400

    Added json

---
+++ GitOut/Controllers/NameTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    public class NameTest : Controller
+    {
+        public JsonResult Name()
+        {
+            string output = false ? "Sydney Jacob" : "Sydney Jacob";
+            return Json(output);
+        }
+        //source used: https://www.c-sharpcorner.com/UploadFile/2ed7ae/jsonresult-type-in-mvc/
+    }
+}
